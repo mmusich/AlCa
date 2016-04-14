0b4b867282370a1ab005ef2741abf430122a24bb
mmusich <marco.musich@cern.ch>
2016-04-14 15:10:16 +0200

    adding Last IOV feature

--- scripts/getGTDiffByIOV.py
+++ scripts/getGTDiffByIOV.py
@@ -30,6 +30,7 @@ def main():
     parser = OptionParser(description=desc,version='%prog version 0.1')
     parser.add_option('-r','--run', help='test run number', dest='testRunNumber', action='store', default='251883')
     parser.add_option('-R','--ReferenceGT',help='Reference Global Tag', dest='refGT', action='store', default='GR_H_V58C')
+    parser.add_option('-L','--last',help='compares the very last IOV' , dest='lastIOV',action='store_true', default=False)
     parser.add_option('-T','--TargetGT'   ,help='Target Global Tag'   , dest='tarGT', action='store', default='74X_dataRun2_HLTValidation_Queue')
     (opts, args) = parser.parse_args()
 
@@ -47,40 +48,61 @@ def main():
         RefTag    = element["tag_name"]
 
         for element2 in myGTtar:
-            if (RefRecord == element2["record"] and  RefLabel==element2["label"]): 
+            if (RefRecord == element2["record"] and RefLabel==element2["label"]): 
                 if RefTag != element2["tag_name"]:
                     differentTags[RefRecord]=(RefTag,element2["tag_name"],RefLabel)
 
     print "| *Record* | *"+opts.refGT+"* | *"+opts.tarGT+"* |"
        
-    for Rcd in differentTags:
+    
+
+    for Rcd in sorted(differentTags):
+
+        #print Rcd,differentTags[Rcd][2]," 1:",differentTags[Rcd][0]," 2:",differentTags[Rcd][1]
+
         refTagIOVs = con.tag(name=differentTags[Rcd][0]).iovs().as_dicts()
         tarTagIOVs = con.tag(name=differentTags[Rcd][1]).iovs().as_dicts()
- 
-        theGoodRefIOV=-1
-        theGoodTarIOV=-1
-        theRefPayload=""
-        theTarPayload=""
-        theRefTime=""
-        theTarTime=""
 
-        for IOV in refTagIOVs:
-            sinceRefTagIOV = IOV["since"]
-            if (sinceRefTagIOV < int(opts.testRunNumber)):
-                theGoodRefIOV=sinceRefTagIOV
-                theRefPayload=IOV["payload_hash"]
-                theRefTime=str(IOV["insertion_time"])
-           
-        for IOV in tarTagIOVs:
-            sinceTarTagIOV = IOV["since"]
-            if (sinceTarTagIOV < int(opts.testRunNumber)):
-                theGoodTarIOV=sinceTarTagIOV
-                theTarPayload=IOV["payload_hash"]
-                theTarTime=str(IOV["insertion_time"])
+        if(opts.lastIOV):
+
+            #print "COMPARING the LAST IOV"
+
+            hash_lastRefTagIOV = refTagIOVs[-1]["payload_hash"]
+            hash_lastTagTagIOV = tarTagIOVs[-1]["payload_hash"]
+
+            time_lastRefTagIOV = str(refTagIOVs[-1]["insertion_time"])
+            time_lastTagTagIOV = str(tarTagIOVs[-1]["insertion_time"])
+
+            if(hash_lastRefTagIOV!=hash_lastTagTagIOV):
+                print "| ="+Rcd+"= ("+differentTags[Rcd][2]+") | =="+differentTags[Rcd][0]+"==  | =="+differentTags[Rcd][1]+"== |"
+                print "|^|"+hash_lastRefTagIOV+" ("+time_lastRefTagIOV+") | "+hash_lastTagTagIOV+" ("+time_lastTagTagIOV+") |"
+
+        else:    
+
+            theGoodRefIOV=-1
+            theGoodTarIOV=-1
+            theRefPayload=""
+            theTarPayload=""
+            theRefTime=""
+            theTarTime=""
+
+            for IOV in refTagIOVs:
+                sinceRefTagIOV = IOV["since"]
+                if (sinceRefTagIOV < int(opts.testRunNumber)):
+                    theGoodRefIOV=sinceRefTagIOV
+                    theRefPayload=IOV["payload_hash"]
+                    theRefTime=str(IOV["insertion_time"])
+          
+            for IOV in tarTagIOVs:
+                sinceTarTagIOV = IOV["since"]
+                if (sinceTarTagIOV < int(opts.testRunNumber)):
+                    theGoodTarIOV=sinceTarTagIOV
+                    theTarPayload=IOV["payload_hash"]
+                    theTarTime=str(IOV["insertion_time"])
         
-        if(theRefPayload!=theTarPayload):
-            print "| ="+Rcd+"= ("+differentTags[Rcd][2]+") | =="+differentTags[Rcd][0]+"==  | =="+differentTags[Rcd][1]+"== |"
-            print "|^|"+theRefPayload+" ("+theRefTime+") | "+theTarPayload+" ("+theTarTime+") |"
+                    if(theRefPayload!=theTarPayload):
+                        print "| ="+Rcd+"= ("+differentTags[Rcd][2]+") | =="+differentTags[Rcd][0]+"==  | =="+differentTags[Rcd][1]+"== |"
+                        print "|^|"+theRefPayload+" ("+theRefTime+") | "+theTarPayload+" ("+theTarTime+") |"
                        
 
 if __name__ == "__main__":        
