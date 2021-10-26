44a8d99ff476a60b93742274950a30e59a1ae34a
mmusich <marco.musich@cern.ch>
2021-10-26 11:39:58 +0200

    push script to retrieve the last IOV of the Tracker Trigger Bits in GT

---
+++ scripts/printLastIOVTrackerTriggerBits.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+#from prettytable import PrettyTable
+import CondCore.Utilities.conddblib as conddb
+#import ConfigParser
+import calendar
+import optparse
+import glob
+import json
+import os,sys
+import re
+import string, re
+import subprocess
+import sys
+import time
+import xml.etree.ElementTree as ET
+
+##############################################
+def getFSCR():
+##############################################
+    out = subprocess.check_output(["curl", "-k", "-s", "https://cmsweb.cern.ch/t0wmadatasvc/prod/firstconditionsaferun"])
+    response = json.loads(out)["result"][0]
+    return int(response)
+
+##############################################
+def getPromptGT():
+##############################################
+    out = subprocess.check_output(["curl", "-k", "-s", "https://cmsweb.cern.ch/t0wmadatasvc/prod/reco_config"])
+    response = json.loads(out)["result"][0]['global_tag']
+    return response
+
+##############################################
+def getExpressGT():
+##############################################
+    out = subprocess.check_output(["curl", "-k", "-s", "https://cmsweb.cern.ch/t0wmadatasvc/prod/express_config"])
+    response = json.loads(out)["result"][0]['global_tag']
+    return response
+
+##############################################
+def getAlCaRecoTriggerBitTag(GTMap):
+##############################################
+    for element in GTMap:
+        Record = element[0]
+        Label  = element[1]
+        Tag    = element[2]
+        if(Record=="AlCaRecoTriggerBitsRcd"):
+            return Tag
+
+        return "NOTATAG"
+
+##############################################
+def getCommandOutput(command):
+##############################################
+    """This function executes `command` and returns it output.
+    Arguments:
+    - `command`: Shell command to be invoked by this function.
+    """
+    child = os.popen(command)
+    data = child.read()
+    err = child.close()
+    if err:
+        print ('%s failed w/ exit code %d' % (command, err))
+    return data
+
+##############################################
+def parseXML(xmlstring):
+##############################################
+    outDict = {}
+    # create element tree object
+    tree = ET.ElementTree(ET.fromstring(xmlstring))
+  
+    # get root element
+    root = tree.getroot()
+
+    for child in root:
+        #print(child.tag, child.attrib)
+        for gc in child:
+            #print(gc.tag, gc.attrib)
+            for ggc in gc:
+                if(ggc.tag=="item"):
+                    #print(ggc.tag)
+                    alca=""
+                    trigger=""
+                    for gggc in ggc:
+                        #print(gggc.tag, gggc.text)
+                        if(gggc.tag=="first"):
+                            alca = gggc.text
+                        if(gggc.tag=="second"):
+                            trigger = gggc.text
+                    outDict[alca]=trigger        
+
+    return outDict
+
+##############################################
+if __name__ == "__main__":
+##############################################
+
+    parser = optparse.OptionParser(usage =
+                                   'Usage: %prog [options] <file> [<file> ...]\n')
+
+    parser.add_option('-E', '--isExpress',
+                      dest = 'express',
+                      default = True,
+                      action="store_false",
+                      help = 'express or prompt')
+    (options, arguments) = parser.parse_args()
+
+    FSCR = getFSCR()
+    promptGT  = getPromptGT()
+    expressGT = getExpressGT() 
+    print("Current FSCR:",FSCR,"| Express Global Tag",expressGT,"| Prompt Global Tag",promptGT,"\n")
+    
+    ####################################
+    # Set up connections with the DB
+    ####################################
+
+    con = conddb.connect(url = conddb.make_url("pro"))
+    session = con.session()
+    IOV     = session.get_dbtype(conddb.IOV)
+    TAG     = session.get_dbtype(conddb.Tag)
+    GT      = session.get_dbtype(conddb.GlobalTag)
+    GTMAP   = session.get_dbtype(conddb.GlobalTagMap)
+    RUNINFO = session.get_dbtype(conddb.RunInfo)
+
+    myGlobalTag = expressGT if options.express else promptGT
+
+    GTMap = session.query(GTMAP.record, GTMAP.label, GTMAP.tag_name).\
+            filter(GTMAP.global_tag_name == myGlobalTag).\
+            order_by(GTMAP.record, GTMAP.label).\
+            all()
+
+    myTag = getAlCaRecoTriggerBitTag(GTMap)
+
+    print("%s GT AlCaRecoTriggerBit Tag: " % ("Express" if options.express else "Prompt") ,myTag)
+
+    myIOVs = session.query(IOV.since,IOV.payload_hash,IOV.insertion_time).filter(IOV.tag_name == myTag).all()
+    myLastIOV = myIOVs[-1]
+    print(myLastIOV,"\n")
+
+    if(options.express):
+        print("Gonna dump the last express Global Tag IOV. Hang on... ")
+    else:
+        print("Gonna dump the last prompt Global Tag IOV. Hang on... ")
+
+    command="conddb dump %s" % myLastIOV[1]
+    theXML= getCommandOutput(command)
+    expressDict = parseXML(theXML)
+
+    print("\n")
+    
+    for key,value in expressDict.items():
+        if ("SiPixel" in key or "SiStrip" in key or "TkAl" in key):
+            print(key,":","[",value,"]")
